45c7b6e00e86f90a2747bc143e8fe5f601675b36
Trond Sørås <trond@dx.no>
2017-12-01 10:20:23 +0100

    Read file, turn into ints, and print
    Ignore build system files

--- megatrond/01/main.hs
+++ megatrond/01/main.hs
@@ -3,9 +3,13 @@
 -- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
 --
 import System.Environment
- 
+
+trim :: String -> String
+trim = reverse . dropWhile(=='\n') . reverse
+
 -- | 'main' runs the main program
 main :: IO ()
-main = getArgs >>= print . haqify . head
- 
-haqify s = "Haq! " ++ s
+main = do
+	content <- readFile("input.txt")
+	let ints = map (\x -> read [x]::Int) (trim content)
+	print ints
