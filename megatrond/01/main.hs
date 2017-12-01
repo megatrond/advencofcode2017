5290ee3edc2d6aa47ef9ccf476a40eeaed167ebc
Trond Sørås <trond@dx.no>
2017-12-01 12:12:01 +0100

    haskell takes some getting used to...

--- megatrond/01/main.hs
+++ megatrond/01/main.hs
@@ -7,9 +7,18 @@ import System.Environment
 trim :: String -> String
 trim = reverse . dropWhile(=='\n') . reverse
 
+returnIfEqual :: Int -> Int -> Int
+returnIfEqual a b = if a == b then a else 0
+
+mySum :: [Int] -> Int -> Int -> Int
+mySum xs length index
+	| length == index = returnIfEqual (xs!!(index-1)) (xs!!0)
+	| otherwise = (returnIfEqual (xs!!(index-1)) (xs!!index)) + mySum xs length (index + 1)
+
 -- | 'main' runs the main program
 main :: IO ()
 main = do
 	content <- readFile("input.txt")
 	let ints = map (\x -> read [x]::Int) (trim content)
-	print ints
+	let l = mySum ints (length ints) 1
+	print l
