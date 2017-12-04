9ec62080dd6f9390287839877a80a2c0a1dd7c01
Trond Sørås <trond@dx.no>
2017-12-04 10:31:47 +0100

    Second star

--- megatrond/01/main.hs
+++ megatrond/01/main.hs
@@ -8,12 +8,20 @@ trim :: String -> String
 trim = reverse . dropWhile(=='\n') . reverse
 
 returnIfEqual :: Int -> Int -> Int
-returnIfEqual a b = if a == b then a else 0
+returnIfEqual a b 
+	| a == b = a
+	| otherwise = 0
 
 mySum :: [Int] -> Int -> Int -> Int
-mySum xs length index
+mySum xs length index 
 	| length == index = returnIfEqual (xs!!(index-1)) (xs!!0)
-	| otherwise = (returnIfEqual (xs!!(index-1)) (xs!!index)) + mySum xs length (index + 1)
+	| otherwise = (returnIfEqual (xs !! (index-1)) (xs !! index)) + (mySum xs length (index + 1))
+
+mySum2 :: [Int] -> Int -> Int -> Int
+mySum2 xs length index
+	| length == index = returnIfEqual (xs!!(index-1)) (xs!!(div length 2))
+	| otherwise = (returnIfEqual (xs !! (index-1)) (xs !! (((index - 1) + (length `div` 2)) `mod` length))) + (mySum2 xs length (index + 1))
+
 
 -- | 'main' runs the main program
 main :: IO ()
@@ -21,4 +29,10 @@ main = do
 	content <- readFile("input.txt")
 	let ints = map (\x -> read [x]::Int) (trim content)
 	let l = mySum ints (length ints) 1
+	print "First star answer:"
 	print l
+	-- content2 <- readFile("input.txt")
+	-- let ints2 = map (\x -> read [x]::Int) (trim content2)
+	let l2 = mySum2 ints (length ints) 1
+	print "Second star answer:"
+	print l2
